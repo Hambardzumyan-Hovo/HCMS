79e60684e044c022e732748380eb83a841e0d7b8
thedarkknight1990 <hovhannessyan@gmail.com>
2020-07-16 16:46:55 +0400

    Updates

---
+++ precache-manifest.6ab3409155cad5d2209e5fb82fa4a6af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557761087a44b935c36da1986ebba6bd",
+    "url": "/HMS/index.html"
+  },
+  {
+    "revision": "df0689c3372b5ddbe424",
+    "url": "/HMS/static/css/main.b015a7eb.chunk.css"
+  },
+  {
+    "revision": "750382483e1f05fc621c",
+    "url": "/HMS/static/js/2.d2efccc1.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/HMS/static/js/2.d2efccc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0689c3372b5ddbe424",
+    "url": "/HMS/static/js/main.66716167.chunk.js"
+  },
+  {
+    "revision": "840d1b0f2208de0c12c2",
+    "url": "/HMS/static/js/runtime-main.973daac0.js"
+  }
+]);
